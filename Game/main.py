e530cf9028a31b526ea9d745ce1fa7f9a03ddebc
D4N23 <daniilosam23@gmail.com>
2023-04-04 10:11:26 -0300

    aplicação do metodo random e das formataçoes de String

---
+++ Game/main.py
@@ -0,0 +1,32 @@
+import random
+print("********************************")
+print(" Bem vindo ao game de advinhação")
+print("********************************")
+
+numero_secreto = round(random.random() * 100)
+total_de_tentativa = 3
+
+for rodada in range(1,total_de_tentativa + 1):
+
+    print("Tentativa {} do total de {}".format(rodada, total_de_tentativa))
+    chute_str = input("Digite um numero(De 1 a 100 seu animal!!!): ")
+    print("Você digitou: ", chute_str)
+    chute = int(chute_str)
+
+    if (chute < 1 or chute > 100):
+        print("Você deve digitar um número entre 1 e 100! seu burro!!!")
+        continue
+
+    acertou = chute == numero_secreto
+    menor = chute < numero_secreto
+    maior = chute > numero_secreto
+
+    if(acertou):
+        print("Você acertou!!!")
+        break
+    else:
+        if(menor):
+            print("Você errou, o valor chutado é menor!")
+        elif(maior):
+            print("Você errou, o valor chutado é maior!")
+print("Fim de Papo o numero correto era o {}".format(numero_secreto))
